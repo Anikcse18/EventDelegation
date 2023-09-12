4d36b1d86c31b79a7cada89a41f681223bce2a02
Anikcse18 <istiakjavedanik@gmail.com>
2023-09-13 02:16:53 +0600

    Add delegation

--- delegation.js
+++ delegation.js
@@ -1,1 +1,24 @@
-!
+
+document.getElementById('list').addEventListener('click', function (event) {
+
+    event.target.parentNode.removeChild(event.target);
+})
+
+
+document.getElementById('add-btn').addEventListener('click', function () {
+
+    var getUl = document.getElementById('list');
+
+    var li = document.createElement('li');
+
+
+    li.classList = 'item';
+    li.innerText = 'Fuck of the deligation';
+    getUl.appendChild(li);
+
+
+
+
+
+})
+
